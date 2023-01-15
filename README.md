4d75c33126973fcc4c12495b430ef0e98d994716
Evan Griffiths <griffithse@gmail.com>
2023-01-15 17:16:44 -0500

    Add summary and shellscript deck picker

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Spaced2
+
 Command-line spaced repetition flashcard reviewer
 
 Flash cards live in the plain text files in "cards" directory
@@ -7,6 +8,32 @@ History is saved in "history"
 
 ### Run
 
+Summary of decks
+
 ```
 cargo run
+```
+
+Quiz a specific deck
+
+```
+cargo run deckname
+```
+
+### Shell alias
+
+Replace `app` path with path to spaced2 executable
+
+```
+sp () {
+  local app
+  app='/Users/sleepylemur/rust/spaced2/target/debug/spaced2'
+  "$app" "$("$app" | fzf | awk '{print $1}')"
+}
+```
+
+build and run
+
+```
+cargo build && sp
 ```
--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,55 @@
 use anyhow::Error;
+use card_selection::get_possible;
+use cards::Card;
 use crossterm::{
-    execute,
+    cursor, execute,
     terminal::{EnterAlternateScreen, LeaveAlternateScreen},
 };
-use std::io;
+use history::History;
+use std::{collections::HashMap, env, fs, io};
 
 mod card_selection;
 mod cards;
 mod history;
 mod quiz;
 
+fn get_summary(filename: &str) -> Result<String, Error> {
+    let mut cards: HashMap<String, Card> = cards::Card::from_file(&format!("cards/{}", filename))?
+        .into_iter()
+        .map(|card| (card.tag.clone(), card))
+        .collect();
+
+    let mut history = History::open(&format!("history/{}", filename))?;
+    history.parse(&mut cards)?;
+    let (_possible, inactive_count, to_review_count) = get_possible(&mut cards, &None);
+    return Ok(format!(
+        "{} total: {} reviewing: {} unlearned: {}",
+        filename,
+        cards.len(),
+        to_review_count,
+        inactive_count
+    ));
+}
+
 fn main() -> Result<(), Error> {
-    let mut stdout = io::stdout();
-    let stdin = io::stdin();
-    execute!(stdout, EnterAlternateScreen)?;
+    let args: Vec<String> = env::args().collect();
+    if args.len() == 1 {
+        let entries = fs::read_dir("cards")?;
+        for entry in entries {
+            println!(
+                "{}",
+                (get_summary(&entry?.file_name().into_string().unwrap())?)
+            );
+        }
+    } else if args[1].len() > 0 {
+        let mut stdout = io::stdout();
+        let stdin = io::stdin();
+        execute!(stdout, EnterAlternateScreen)?;
 
-    quiz::quiz("ex", &mut stdout, &stdin)?;
+        quiz::quiz(&args[1], &mut stdout, &stdin)?;
 
-    execute!(stdout, LeaveAlternateScreen)?;
+        execute!(stdout, cursor::Show)?;
+        execute!(stdout, LeaveAlternateScreen)?;
+    }
     Ok(())
 }

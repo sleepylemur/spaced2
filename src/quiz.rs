cea0c16591ae7ac03c104355ac80a7ef5b8c2222
Evan Griffiths <griffithse@gmail.com>
2023-01-15 18:26:25 -0500

    re-quiz on mistake

--- src/quiz.rs
+++ src/quiz.rs
@@ -1,7 +1,8 @@
 use std::{
     collections::HashMap,
     io::{Stdin, Stdout},
-    time::{SystemTime, UNIX_EPOCH},
+    thread,
+    time::{Duration, SystemTime, UNIX_EPOCH},
 };
 
 use anyhow::Error;
@@ -39,9 +40,10 @@ pub fn quiz(filename: &str, stdout: &mut Stdout, stdin: &Stdin) -> Result<(), Er
 
     let mut answer = String::new();
     let mut last_tag: Option<String> = None;
-    let mut last_correct = None;
+    let mut is_correct = true;
+    let mut quitting = false;
     execute!(stdout, Clear(ClearType::All), cursor::MoveTo(0, 0))?;
-    loop {
+    while !quitting {
         let (possible, inactive_count, to_review_count) = get_possible(&mut cards, &last_tag);
         if to_review_count == 0 {
             if inactive_count == 0 {
@@ -53,6 +55,7 @@ pub fn quiz(filename: &str, stdout: &mut Stdout, stdin: &Stdin) -> Result<(), Er
                     "All cards reviewed, {} unlearned.\na to add. anything else to quit.",
                     inactive_count
                 );
+                answer.truncate(0);
                 stdin.read_line(&mut answer)?;
                 if answer.trim() == "a" {
                     activate_cards(&mut cards, 5);
@@ -61,39 +64,51 @@ pub fn quiz(filename: &str, stdout: &mut Stdout, stdin: &Stdin) -> Result<(), Er
                 }
             }
         } else if let Some(card) = random_card(&possible, &mut cards) {
-            match last_correct {
-                Some(true) => println!("correct"),
-                Some(false) => println!("wrong"),
-                None => (),
+            loop {
+                if !is_correct {
+                    println!(
+                        "wrong\n{}\n{}\npress enter to continue",
+                        card.question, card.answer
+                    );
+                    stdin.read_line(&mut answer)?;
+                    if answer.len() == 0 {
+                        quitting = true;
+                        break;
+                    }
+                    is_correct = true; // skip to re-asking question
+                } else {
+                    println!(
+                        "reviewing {}, unlearned {}",
+                        to_review_count, inactive_count
+                    );
+                    println!("{}", card.question);
+                    stdin.read_line(&mut answer)?;
+                    if answer.len() == 0 {
+                        quitting = true;
+                        break;
+                    }
+
+                    is_correct = if answer.trim() == card.answer {
+                        true
+                    } else {
+                        false
+                    };
+                    card.update(current_timestamp()?, is_correct, &last_tag, history.num);
+                    history.persist_update(card, is_correct)?;
+                    if is_correct {
+                        answer.truncate(0);
+                        execute!(stdout, Clear(ClearType::All), cursor::MoveTo(0, 0))?;
+                        break;
+                    }
+                }
+                answer.truncate(0);
+                execute!(stdout, Clear(ClearType::All), cursor::MoveTo(0, 0))?;
             }
-            println!(
-                "reviewing {}, unlearned {}",
-                to_review_count, inactive_count
-            );
-            println!("{}", card.question);
-            stdin.read_line(&mut answer)?;
-            if answer.len() == 0 {
-                break;
-            }
-
-            let is_correct = if answer.trim() == card.answer {
-                println!("correct!");
-                true
-            } else {
-                println!("nope");
-                false
-            };
-
-            card.update(current_timestamp()?, is_correct, &last_tag, history.num);
-            history.persist_update(card, is_correct)?;
-
+            println!("correct!");
             last_tag = Some(card.tag.clone());
-            last_correct = Some(is_correct);
         } else {
             break;
         }
-        answer.truncate(0);
-        execute!(stdout, Clear(ClearType::All), cursor::MoveTo(0, 0))?;
     }
     Ok(())
 }
